d20eaf50a564e46d38896106590a0ab268e0e5e9
ichi-h <85932406+ichi-h@users.noreply.github.com>
2025-10-14 12:30:33 +0900

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Bun Environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        packages = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells = {
+          default = packages.mkShell {
+            buildInputs = with packages; [
+              bun
+              biome
+            ];
+          };
+        };
+      }
+    );
+}
