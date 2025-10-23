bb1c3ff65afd5e6a10f7f6e9a62c65a11f79f64b
ichi-h <85932406+ichi-h@users.noreply.github.com>
2025-10-23 11:19:15 +0900

    Add driver package.

---
+++ packages/driver/build.ts
@@ -0,0 +1,29 @@
+import type { BuildConfig } from 'bun';
+import dts from 'bun-plugin-dts';
+
+const shouldMinify = process.argv.includes('--minify');
+
+const defaultBuildConfig: BuildConfig = {
+  entrypoints: ['./src/index.ts'],
+  outdir: './dist',
+};
+
+await Promise.all([
+  Bun.build({
+    ...defaultBuildConfig,
+    plugins: [dts()],
+    format: 'esm',
+    naming: '[dir]/[name].js',
+    minify: shouldMinify,
+    sourcemap: true,
+    external: ["react", "react-dom", "engine"],
+  }),
+  Bun.build({
+    ...defaultBuildConfig,
+    format: 'cjs',
+    naming: '[dir]/[name].cjs',
+    minify: shouldMinify,
+    sourcemap: true,
+    external: ["react", "react-dom", "engine"],
+  }),
+]);
---
+++ packages/driver/src/react/novel-widget-driver.tsx
@@ -0,0 +1,9 @@
+import type { NovelWidget } from 'engine';
+
+interface Props {
+  widgets: NovelWidget[];
+}
+
+export const NovelWidgetDriver = ({ widgets }: Props) => {
+  return <div></div>;
+};
