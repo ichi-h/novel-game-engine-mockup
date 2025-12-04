fdc8c899a981903a6efcd6f03ea1ac2467a2576b
ichi-h <85932406+ichi-h@users.noreply.github.com>
2025-12-04 13:29:14 +0900

    Launch new example project.

---
+++ examples/tutorial/src/frontend.tsx
@@ -0,0 +1,32 @@
+/**
+ * This file is the entry point for the React app, it sets up the root
+ * element and renders the App component to the DOM.
+ *
+ * It is included in `src/index.html`.
+ */
+
+import { resumeAudioContext } from 'driver';
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+import { initMixerDriver } from './libs/mixer-driver';
+
+function start() {
+  initMixerDriver();
+
+  const container = document.getElementById('root');
+  if (!container) {
+    throw new Error("Root element with id 'root' not found");
+  }
+  const root = createRoot(container);
+  root.render(<App />);
+
+  document.addEventListener('click', async () => {
+    await resumeAudioContext();
+  });
+}
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', start);
+} else {
+  start();
+}
