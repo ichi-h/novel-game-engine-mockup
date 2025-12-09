705315c3f57542de871ca3b89736fd75a28b4726
ichi-h <85932406+ichi-h@users.noreply.github.com>
2025-12-09 19:10:37 +0900

    Export all items from mixer.

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,16 +1,4 @@
-export type {
-  ApplyMixer,
-  BusTrack,
-  Channel,
-  DelayMs,
-  FadeInMs,
-  FadeOutMs,
-  Mixer,
-  OffsetMs,
-  Samples,
-  Track,
-  Volume,
-} from './mixer';
+export * as m from './mixer';
 export * from './model';
 export * from './store';
 export { w } from './ui';
