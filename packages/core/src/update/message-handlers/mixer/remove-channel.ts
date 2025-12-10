9cab17429e0c3be724812e33b6236070b994b626
ichi-h <85932406+ichi-h@users.noreply.github.com>
2025-12-10 20:47:31 +0900

    Remove hasId check in handleRemoveChannel

--- packages/core/src/update/message-handlers/mixer/remove-channel.ts
+++ packages/core/src/update/message-handlers/mixer/remove-channel.ts
@@ -1,5 +1,5 @@
 import type { BaseMessage, ReturnModel, Update } from '@ichi-h/elmish';
-import { filter, hasId } from '@/mixer';
+import { filter } from '@/mixer';
 import type { NovelModel } from '@/model';
 import type { NovelMessage } from '@/update/message';
 
@@ -20,15 +20,6 @@ export const handleRemoveChannel = <CustomState = unknown>(
   msg: RemoveChannelMessage,
   update: Update<NovelModel<CustomState>, NovelMessage<CustomState>>,
 ): ReturnModel<NovelModel<CustomState>, NovelMessage<CustomState>> => {
-  if (!hasId(model.mixer.value, msg.channelId)) {
-    return update(model, {
-      type: 'Error',
-      value: new Error(
-        `Channel with ID ${msg.channelId} does not exist in the mixer.`,
-      ),
-    });
-  }
-
   const mixer = filter((c) => c.id !== msg.channelId)(model.mixer.value);
 
   return update(
